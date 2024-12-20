2f551f63cfd44bdaf800a85fd6b23868d50be7bf
gjg525 <guen.grosklos@speedgoat.io>
2024-12-20 15:34:45 -0700

    Package documentation

---
+++ R/spdgt.secr.host.R
@@ -0,0 +1,6 @@
+#' @description
+#' Run mark-recapture models
+#' @importFrom stats dbinom dnorm dpois pnorm rbeta rbinom rgamma rnorm runif
+#' @importFrom foreach %dopar%
+#' @importFrom rlang .data
+"_PACKAGE"
